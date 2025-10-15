bfd5a5c07e25ac3f78634bcbc4aee702f22fa817
hyoseok <alexization@gmail.com>
2025-10-15 20:53:02 +0900

    feat: 기본적인 회원 조회 JPA 레포지토리 생성

---
+++ src/main/kotlin/com/example/oauth/repository/UserRepository.kt
@@ -0,0 +1,10 @@
+package com.example.oauth.repository
+
+import com.example.oauth.domain.User
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface UserRepository : JpaRepository<User, Long> {
+    fun findByEmail(email: String): User?
+
+    fun existsByEmail(email: String): Boolean
+}
