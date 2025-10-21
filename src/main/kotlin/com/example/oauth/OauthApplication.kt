e9227c6e5f4386b3444a88852a1c578cfad5a4e9
hyoseok <alexization@gmail.com>
2025-10-21 16:47:01 +0900

    feat: CreatedDate, LastModifiedDate 사용을 위한 EnableJpaAuditing 추가

--- src/main/kotlin/com/example/oauth/OauthApplication.kt
+++ src/main/kotlin/com/example/oauth/OauthApplication.kt
@@ -2,8 +2,10 @@ package com.example.oauth
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @SpringBootApplication
+@EnableJpaAuditing
 class OauthApplication
 
 fun main(args: Array<String>) {
